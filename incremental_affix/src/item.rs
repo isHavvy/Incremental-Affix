1c3b3701e276b5ea5fa5aeb6a91322aa29cb173e
isHavvy <is.havvy@gmail.com>
2024-09-07 23:17:28 -0700

    More item stuff.
    Can generate random bases and attach random affixes to it. No real
    idea of what a world is, but the drop tables are stored in an AnyMap for now.

--- incremental_affix/src/item.rs
+++ incremental_affix/src/item.rs
@@ -1,9 +1,14 @@
-use std::ops::Deref;
+use std::ops::{Deref, DerefMut};
+
+use anymap::AnyMap;
+use rand::Rng as _;
 
 #[derive(Debug, Clone, Copy)]
 pub(crate) enum ModifierKind {
     InventoryBase,
     InventoryHeight,
+    IncreasedVolume,
+    InventorySkillGain,
 }
 
 #[derive(Debug, Clone, Copy)]
@@ -22,7 +27,13 @@ impl Modifier {
         match self.kind {
             ModifierKind::InventoryBase => format!("{}{} Inventory Base", sign(actual), actual),
             ModifierKind::InventoryHeight => format!("{}{} Inventory Height", sign(actual), actual),
+            ModifierKind::IncreasedVolume => format!("{}{}% Increased Volume", sign(actual), actual),
+            ModifierKind::InventorySkillGain => format!("Skills in inventory gain {}% of earned experience", actual),
         }
+    }
+
+    fn random_modifier_value(&self) -> i32 {
+        rand::thread_rng().gen_range(self.min..self.max + 1)
     }
 }
 
@@ -36,6 +47,17 @@ pub struct Affix {
 }
 
 impl Affix {
+    /// Construct a new affix with the given modifier. Sets the hybrid to `None`.
+    fn new(name: String, modifier: Modifier) -> Self {
+        Self {
+            name,
+            modifier,
+            modifier_actual: if modifier.min == modifier.max { modifier.min } else { 0 },
+            hybrid_modifier: None,
+            hybrid_modifier_actual: 0,
+        }
+    }
+
     fn display(&self) -> String {
         let mut output = String::new();
 
@@ -46,6 +68,13 @@ impl Affix {
         }
 
         output
+    }
+
+    pub(crate) fn randomize_actual(&mut self) {
+        self.modifier_actual = self.modifier.random_modifier_value();
+        if let Some(modifier) = self.hybrid_modifier {
+            self.hybrid_modifier_actual = modifier.random_modifier_value();
+        }
     }
 }
 
@@ -60,11 +89,45 @@ impl Deref for Implicit {
     }
 }
 
-#[derive(Debug)]
+impl DerefMut for Implicit {
+    fn deref_mut(&mut self) -> &mut Self::Target {
+        &mut self.0
+    }
+}
+
+#[derive(Debug, Clone)]
 pub(crate) struct Prefix(Affix);
 
-#[derive(Debug)]
+impl Deref for Prefix {
+    type Target = Affix;
+
+    fn deref(&self) -> &Self::Target {
+        &self.0
+    }
+}
+
+impl DerefMut for Prefix {
+    fn deref_mut(&mut self) -> &mut Self::Target {
+        &mut self.0
+    }
+}
+
+#[derive(Debug, Clone)]
 pub(crate) struct Suffix(Affix);
+
+impl Deref for Suffix {
+    type Target = Affix;
+
+    fn deref(&self) -> &Self::Target {
+        &self.0
+    }
+}
+
+impl DerefMut for Suffix {
+    fn deref_mut(&mut self) -> &mut Self::Target {
+        &mut self.0
+    }
+}
 
 #[derive(Debug, Clone, Copy, Eq, PartialEq)]
 pub(crate) enum Quality {
@@ -78,7 +141,7 @@ pub(crate) enum Quality {
 }
 
 #[derive(Debug, Clone, Copy, Eq, PartialEq, Hash)]
-struct AffixiveItemBaseIndex(usize);
+pub(crate) struct AffixiveItemBaseIndex(usize);
 
 #[derive(Debug, Clone, Copy, Eq, PartialEq, Hash)]
 struct ImplicitIndex(usize);
@@ -95,9 +158,15 @@ pub(crate) struct AffixiveItemBase {
     implicits: Vec<ImplicitIndex>,
 }
 
+#[derive(Debug, Clone, Copy)]
+pub(crate) enum PushAffixError {
+    AffixiveItemIsFixed,
+    AffixiveItemQualityTooLow,
+}
+
 #[derive(Debug)]
 pub(crate) struct AffixiveItem {
-    base: AffixiveItemBaseIndex,
+    base_ix: AffixiveItemBaseIndex,
     implicits: Vec<Implicit>,
     prefixes: Vec<Prefix>,
     suffixes: Vec<Suffix>,
@@ -105,10 +174,22 @@ pub(crate) struct AffixiveItem {
 }
 
 impl AffixiveItem {
-    fn display(&self, implicits: &[Implicit], bases: &[AffixiveItemBase]) -> String {
+    pub(crate) fn new(bases: &[AffixiveItemBase], implicits: &[Implicit], base_ix: AffixiveItemBaseIndex, quality: Quality) -> Self {
+        let base = &bases[base_ix.0];
+        let implicits = base.implicits.iter().map(|ix| implicits[ix.0].clone()).collect();
+
+        Self {
+            base_ix,
+            implicits,
+            prefixes: vec![],
+            suffixes: vec![],
+            quality,
+        }
+    }
+    pub(crate) fn display(&self, bases: &[AffixiveItemBase]) -> String {
         let mut output = String::new();
 
-        let name: &str = &bases[self.base.0].name;
+        let name: &str = &bases[self.base_ix.0].name;
 
         match self.quality {
             Quality::FixedArtifact => {
@@ -136,11 +217,57 @@ impl AffixiveItem {
 
         output.push_str("\n===\n");
 
+        let mut has_affix = false;
+
+        for prefix in self.prefixes.iter() {
+            has_affix = true;
+            output.push_str(&prefix.display());
+            output.push('\n');
+        }
+
+        for suffix in self.suffixes.iter() {
+            has_affix = true;
+            output.push_str(&suffix.display());
+            output.push('\n');
+        }
+
+        if has_affix {
+            output.push_str("===\n");
+        }
+
         output
+    }
+
+    /// Attempt to attach a prefix to this item.
+    /// 
+    /// Return Ok(()) if the prefix was added.
+    pub(crate) fn try_push_prefix(&mut self, prefix: Prefix) -> Result<(), PushAffixError> {
+        match self.quality {
+            Quality::FixedArtifact => Err(PushAffixError::AffixiveItemIsFixed),
+            Quality::Quality(quality) if quality as usize == self.prefixes.len() => Err(PushAffixError::AffixiveItemQualityTooLow),
+            _ => {
+                self.prefixes.push(prefix);
+                Ok(())
+            }
+        }
+    }
+
+    /// Attempt to attach a suffix to this item.
+    /// 
+    /// Return Ok(()) if the suffix was added.
+    pub(crate) fn try_push_suffix(&mut self, suffix: Suffix) -> Result<(), PushAffixError> {
+        match self.quality {
+            Quality::FixedArtifact => Err(PushAffixError::AffixiveItemIsFixed),
+            Quality::Quality(quality) if quality as usize == self.suffixes.len() => Err(PushAffixError::AffixiveItemQualityTooLow),
+            _ => {
+                self.suffixes.push(suffix);
+                Ok(())
+            }
+        }
     }
 }
 
-fn initialize_bases() -> Vec<AffixiveItemBase> {
+pub(crate) fn initialize_bases() -> Vec<AffixiveItemBase> {
     let mut bases = vec![];
 
     let inventory_tags = vec![AffixiveItemBaseTag::Inventory];
@@ -184,7 +311,7 @@ fn initialize_bases() -> Vec<AffixiveItemBase> {
     bases
 }
 
-fn initialize_implicits() -> Vec<Implicit> {
+pub(crate) fn initialize_implicits() -> Vec<Implicit> {
     let mut mods = vec![];
 
     mods.push(Affix {
@@ -238,7 +365,7 @@ fn initialize_implicits() -> Vec<Implicit> {
     mods.into_iter().map(Implicit).collect()
 }
 
-fn initialize_suffixes() -> Vec<Suffix> {
+pub(crate) fn initialize_suffixes() -> Vec<Suffix> {
     let mut suffixes = vec![];
 
     suffixes.push(Affix {
@@ -257,23 +384,156 @@ fn initialize_suffixes() -> Vec<Suffix> {
         hybrid_modifier_actual: 0,
     });
 
+    suffixes.push(Affix::new("Tiny Pockets".to_string(), Modifier { kind: ModifierKind::IncreasedVolume, min: 8, max: 15 }));
+    suffixes.push(Affix::new("Pockets".to_string(), Modifier { kind: ModifierKind::IncreasedVolume, min: 16, max: 24 }));
+    suffixes.push(Affix::new("Large Tiny Pockets".to_string(), Modifier { kind: ModifierKind::IncreasedVolume, min: 25, max: 34 }));
+    suffixes.push(Affix::new("Gargantuan Pockets".to_string(), Modifier { kind: ModifierKind::IncreasedVolume, min: 35, max: 45 }));
+
     return suffixes.into_iter().map(Suffix).collect()
 }
 
-pub fn make_backpack() -> (Vec<Implicit>, Vec<AffixiveItemBase>, AffixiveItem) {
-    let implicits = initialize_implicits();
-    let bases = initialize_bases();
+pub(crate) fn initialize_prefixes() -> Vec<Prefix> {
+    let mut prefixes = vec![];
 
-    let backpack_base = &bases[3];
+    prefixes.push(Affix::new("Student's".to_string(), Modifier { kind: ModifierKind::InventorySkillGain, min: 1, max: 25 }));
+    prefixes.push(Affix::new("Teacher's".to_string(), Modifier { kind: ModifierKind::InventorySkillGain, min: 26, max: 50 }));
+    prefixes.push(Affix::new("Professor's".to_string(), Modifier { kind: ModifierKind::InventorySkillGain, min: 51, max: 75 }));
+    prefixes.push(Affix::new("Mentor's".to_string(), Modifier { kind: ModifierKind::InventorySkillGain, min: 76, max: 100 }));
 
-    let backpack = AffixiveItem {
-        base: AffixiveItemBaseIndex(3),
-        implicits: backpack_base.implicits.iter().map(|ix| implicits[ix.0].clone()).collect(),
-        prefixes: vec![],
-        suffixes: vec![],
-        quality: Quality::Quality(0),
-    };
+    return prefixes.into_iter().map(Prefix).collect();
+}
 
-    println!("{}", backpack.display(&implicits, &bases));
-    (implicits, bases, backpack)
+pub struct DropTable<T> {
+    total_weight: i32,
+    weights: Vec<i32>,
+    modifiers: Vec<T>,
+}
+
+impl<T: Clone> DropTable<T> {
+    pub fn random(&self) -> T {
+        let r = rand::thread_rng().gen_range(0..self.total_weight);
+
+        let ix = self.weights.partition_point(|weight| weight <= &r);
+        self.modifiers[ix].clone()
+    }
+}
+
+struct DropTableBuilder<T> {
+    total_weight: i32,
+    weights: Vec<i32>,
+    modifiers: Vec<T>,
+}
+
+impl<T: Clone> DropTableBuilder<T> {
+    fn new() -> Self {
+        Self {
+            total_weight: 0,
+            weights: vec![],
+            modifiers: vec![],
+        }
+    }
+
+    fn build(self) -> DropTable<T> {
+        DropTable {
+            total_weight: self.total_weight,
+            weights: self.weights,
+            modifiers: self.modifiers,
+        }
+    }
+
+    fn add_loot(mut self, loot: T, weight: i32) -> Self {
+        self.modifiers.push(loot);
+        self.weights.push(self.total_weight + weight);
+        
+        Self {
+            total_weight: self.total_weight + weight,
+            weights: self.weights,
+            modifiers: self.modifiers,
+        }
+    }
+}
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+pub enum SuffixOrPrefix {
+    Suffix,
+    Prefix,
+}
+
+pub(crate) struct InventoryModifierPrefixes(DropTable<Prefix>);
+
+impl Deref for InventoryModifierPrefixes {
+    type Target = DropTable<Prefix>;
+
+    fn deref(&self) -> &Self::Target {
+        &self.0
+    }
+}
+
+pub(crate) struct InventoryModifierSuffixes(DropTable<Suffix>);
+
+impl Deref for InventoryModifierSuffixes {
+    type Target = DropTable<Suffix>;
+
+    fn deref(&self) -> &Self::Target {
+        &self.0
+    }
+}
+
+pub struct SuffixOrPrefixDropTable(DropTable<SuffixOrPrefix>);
+
+impl Deref for SuffixOrPrefixDropTable {
+    type Target = DropTable<SuffixOrPrefix>;
+
+    fn deref(&self) -> &Self::Target {
+        &self.0
+    }
+}
+
+pub(crate) struct StorageBasesDropTable(DropTable<AffixiveItemBaseIndex>);
+
+impl Deref for StorageBasesDropTable {
+    type Target = DropTable<AffixiveItemBaseIndex>;
+
+    fn deref(&self) -> &Self::Target {
+        &self.0
+    }
+}
+
+pub fn initialize_drop_tables(bases: &[AffixiveItemBase], prefixes: Vec<Prefix>, suffixes: Vec<Suffix>) -> AnyMap {
+    let mut drop_tables = AnyMap::new();
+
+    let mut base_drop_table = DropTableBuilder::new();
+    for (ix, _) in bases.iter().enumerate() {
+        base_drop_table = base_drop_table.add_loot(AffixiveItemBaseIndex(ix), 50);
+    }
+    let base_drop_table = base_drop_table.build();
+
+    drop_tables.insert(StorageBasesDropTable(base_drop_table));
+
+    let prefix_drop_table = DropTableBuilder::new()
+        .add_loot(prefixes[0].clone(), 50)
+        .add_loot(prefixes[1].clone(), 50)
+        .add_loot(prefixes[2].clone(), 50)
+        .add_loot(prefixes[3].clone(), 50)
+        .build();
+
+    drop_tables.insert(InventoryModifierPrefixes(prefix_drop_table));
+
+    let suffix_drop_table = DropTableBuilder::new()
+        .add_loot(suffixes[0].clone(), 50)
+        .add_loot(suffixes[1].clone(), 50)
+        .add_loot(suffixes[2].clone(), 50)
+        .add_loot(suffixes[3].clone(), 50)
+        .build();
+
+    drop_tables.insert(InventoryModifierSuffixes(suffix_drop_table));
+
+    let prefix_or_suffix_table = DropTableBuilder::new()
+        .add_loot(SuffixOrPrefix::Suffix, 1)
+        .add_loot(SuffixOrPrefix::Prefix, 1)
+        .build();
+
+    drop_tables.insert(SuffixOrPrefixDropTable(prefix_or_suffix_table));
+
+    return drop_tables;
 }
--- incremental_affix/src/main.rs
+++ incremental_affix/src/main.rs
@@ -1,5 +1,38 @@
+use item::SuffixOrPrefix;
+
 mod item;
 
 fn main() {
-    let (implicits, bases, backpack) = item::make_backpack();
+    let implicits = item::initialize_implicits();
+    let bases = item::initialize_bases();
+
+    let prefixes = item::initialize_prefixes();
+    let suffixes = item::initialize_suffixes();
+    let drop_tables = item::initialize_drop_tables(&bases, prefixes, suffixes);
+
+    let bases_table = drop_tables.get::<item::StorageBasesDropTable>().unwrap();
+    let base_ix = bases_table.random();
+
+    let mut storage = item::AffixiveItem::new(&bases, &implicits, base_ix, item::Quality::Quality(1));
+    
+    let suffix_or_prefix_table = drop_tables.get::<item::SuffixOrPrefixDropTable>().unwrap();
+    match suffix_or_prefix_table.random() {
+        SuffixOrPrefix::Prefix => {
+            let prefix_table = drop_tables.get::<item::InventoryModifierPrefixes>().unwrap();
+            let mut prefix = prefix_table.random();
+            prefix.randomize_actual();
+
+            let _ = storage.try_push_prefix(prefix);
+        },
+
+        SuffixOrPrefix::Suffix => {
+            let suffix_table = drop_tables.get::<item::InventoryModifierSuffixes>().unwrap();
+            let mut suffix = suffix_table.random();
+            suffix.randomize_actual();
+
+            let _ = storage.try_push_suffix(suffix);
+        }
+    }
+
+    println!("{}", storage.display(&bases[..]));
 }
