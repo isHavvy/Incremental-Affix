bffff63a331f5a0c5ce7c1ab9bb72741a1e5f7b2
isHavvy <is.havvy@gmail.com>
2024-09-08 21:23:05 -0700

    Move Drop Tables to own file.

--- incremental_affix/src/item.rs
+++ incremental_affix/src/item.rs
@@ -1,7 +1,6 @@
 use std::ops::{Deref, DerefMut};
 
-use anymap::AnyMap;
-use rand::Rng as _;
+use rand::Rng;
 
 #[derive(Debug, Clone, Copy)]
 pub(crate) enum ModifierKind {
@@ -141,7 +140,7 @@ pub(crate) enum Quality {
 }
 
 #[derive(Debug, Clone, Copy, Eq, PartialEq, Hash)]
-pub(crate) struct AffixiveItemBaseIndex(usize);
+pub(crate) struct AffixiveItemBaseIndex(pub usize);
 
 #[derive(Debug, Clone, Copy, Eq, PartialEq, Hash)]
 struct ImplicitIndex(usize);
@@ -401,139 +400,4 @@ pub(crate) fn initialize_prefixes() -> Vec<Prefix> {
     prefixes.push(Affix::new("Mentor's".to_string(), Modifier { kind: ModifierKind::InventorySkillGain, min: 76, max: 100 }));
 
     return prefixes.into_iter().map(Prefix).collect();
-}
-
-pub struct DropTable<T> {
-    total_weight: i32,
-    weights: Vec<i32>,
-    modifiers: Vec<T>,
-}
-
-impl<T: Clone> DropTable<T> {
-    pub fn random(&self) -> T {
-        let r = rand::thread_rng().gen_range(0..self.total_weight);
-
-        let ix = self.weights.partition_point(|weight| weight <= &r);
-        self.modifiers[ix].clone()
-    }
-}
-
-struct DropTableBuilder<T> {
-    total_weight: i32,
-    weights: Vec<i32>,
-    modifiers: Vec<T>,
-}
-
-impl<T: Clone> DropTableBuilder<T> {
-    fn new() -> Self {
-        Self {
-            total_weight: 0,
-            weights: vec![],
-            modifiers: vec![],
-        }
-    }
-
-    fn build(self) -> DropTable<T> {
-        DropTable {
-            total_weight: self.total_weight,
-            weights: self.weights,
-            modifiers: self.modifiers,
-        }
-    }
-
-    fn add_loot(mut self, loot: T, weight: i32) -> Self {
-        self.modifiers.push(loot);
-        self.weights.push(self.total_weight + weight);
-        
-        Self {
-            total_weight: self.total_weight + weight,
-            weights: self.weights,
-            modifiers: self.modifiers,
-        }
-    }
-}
-
-#[derive(Debug, Clone, Copy, PartialEq, Eq)]
-pub enum SuffixOrPrefix {
-    Suffix,
-    Prefix,
-}
-
-pub(crate) struct InventoryModifierPrefixes(DropTable<Prefix>);
-
-impl Deref for InventoryModifierPrefixes {
-    type Target = DropTable<Prefix>;
-
-    fn deref(&self) -> &Self::Target {
-        &self.0
-    }
-}
-
-pub(crate) struct InventoryModifierSuffixes(DropTable<Suffix>);
-
-impl Deref for InventoryModifierSuffixes {
-    type Target = DropTable<Suffix>;
-
-    fn deref(&self) -> &Self::Target {
-        &self.0
-    }
-}
-
-pub struct SuffixOrPrefixDropTable(DropTable<SuffixOrPrefix>);
-
-impl Deref for SuffixOrPrefixDropTable {
-    type Target = DropTable<SuffixOrPrefix>;
-
-    fn deref(&self) -> &Self::Target {
-        &self.0
-    }
-}
-
-pub(crate) struct StorageBasesDropTable(DropTable<AffixiveItemBaseIndex>);
-
-impl Deref for StorageBasesDropTable {
-    type Target = DropTable<AffixiveItemBaseIndex>;
-
-    fn deref(&self) -> &Self::Target {
-        &self.0
-    }
-}
-
-pub fn initialize_drop_tables(bases: &[AffixiveItemBase], prefixes: Vec<Prefix>, suffixes: Vec<Suffix>) -> AnyMap {
-    let mut drop_tables = AnyMap::new();
-
-    let mut base_drop_table = DropTableBuilder::new();
-    for (ix, _) in bases.iter().enumerate() {
-        base_drop_table = base_drop_table.add_loot(AffixiveItemBaseIndex(ix), 50);
-    }
-    let base_drop_table = base_drop_table.build();
-
-    drop_tables.insert(StorageBasesDropTable(base_drop_table));
-
-    let prefix_drop_table = DropTableBuilder::new()
-        .add_loot(prefixes[0].clone(), 50)
-        .add_loot(prefixes[1].clone(), 50)
-        .add_loot(prefixes[2].clone(), 50)
-        .add_loot(prefixes[3].clone(), 50)
-        .build();
-
-    drop_tables.insert(InventoryModifierPrefixes(prefix_drop_table));
-
-    let suffix_drop_table = DropTableBuilder::new()
-        .add_loot(suffixes[0].clone(), 50)
-        .add_loot(suffixes[1].clone(), 50)
-        .add_loot(suffixes[2].clone(), 50)
-        .add_loot(suffixes[3].clone(), 50)
-        .build();
-
-    drop_tables.insert(InventoryModifierSuffixes(suffix_drop_table));
-
-    let prefix_or_suffix_table = DropTableBuilder::new()
-        .add_loot(SuffixOrPrefix::Suffix, 1)
-        .add_loot(SuffixOrPrefix::Prefix, 1)
-        .build();
-
-    drop_tables.insert(SuffixOrPrefixDropTable(prefix_or_suffix_table));
-
-    return drop_tables;
 }
--- incremental_affix/src/main.rs
+++ incremental_affix/src/main.rs
@@ -1,6 +1,5 @@
-use item::SuffixOrPrefix;
-
 mod item;
+mod drop_table;
 
 fn main() {
     let implicits = item::initialize_implicits();
@@ -8,25 +7,25 @@ fn main() {
 
     let prefixes = item::initialize_prefixes();
     let suffixes = item::initialize_suffixes();
-    let drop_tables = item::initialize_drop_tables(&bases, prefixes, suffixes);
+    let drop_tables = drop_table::initialize_drop_tables(&bases, prefixes, suffixes);
 
-    let bases_table = drop_tables.get::<item::StorageBasesDropTable>().unwrap();
+    let bases_table = drop_tables.get::<drop_table::StorageBasesDropTable>().unwrap();
     let base_ix = bases_table.random();
 
     let mut storage = item::AffixiveItem::new(&bases, &implicits, base_ix, item::Quality::Quality(1));
     
-    let suffix_or_prefix_table = drop_tables.get::<item::SuffixOrPrefixDropTable>().unwrap();
+    let suffix_or_prefix_table = drop_tables.get::<drop_table::SuffixOrPrefixDropTable>().unwrap();
     match suffix_or_prefix_table.random() {
-        SuffixOrPrefix::Prefix => {
-            let prefix_table = drop_tables.get::<item::InventoryModifierPrefixes>().unwrap();
+        drop_table::SuffixOrPrefix::Prefix => {
+            let prefix_table = drop_tables.get::<drop_table::InventoryModifierPrefixes>().unwrap();
             let mut prefix = prefix_table.random();
             prefix.randomize_actual();
 
             let _ = storage.try_push_prefix(prefix);
         },
 
-        SuffixOrPrefix::Suffix => {
-            let suffix_table = drop_tables.get::<item::InventoryModifierSuffixes>().unwrap();
+        drop_table::SuffixOrPrefix::Suffix => {
+            let suffix_table = drop_tables.get::<drop_table::InventoryModifierSuffixes>().unwrap();
             let mut suffix = suffix_table.random();
             suffix.randomize_actual();
 
